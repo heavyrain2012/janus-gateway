a919f023de4ecaf57828e00b4fb827e0668a042f
heavyrain2012 <heavyrain.lee@gmail.com>
2020-06-08 11:09:56 +0800

    add get im node function

--- transports/janus_mqtt.c
+++ transports/janus_mqtt.c
@@ -27,6 +27,16 @@
  */
 
 #include "transport.h"
+#include <arpa/inet.h>
+#include <assert.h>
+#include <netdb.h> /* getprotobyname */
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #include <MQTTAsync.h>
 
@@ -59,6 +69,7 @@ int janus_mqtt_send_message(janus_transport_session *transport, void *request_id
 void janus_mqtt_session_created(janus_transport_session *transport, guint64 session_id);
 void janus_mqtt_session_over(janus_transport_session *transport, guint64 session_id, gboolean timeout, gboolean claimed);
 void janus_mqtt_session_claimed(janus_transport_session *transport, guint64 session_id);
+int getNodeHost(const char* host, const char* client, char* content);
 
 #define JANUS_MQTT_VERSION_3_1          "3.1"
 #define JANUS_MQTT_VERSION_3_1_1        "3.1.1"
@@ -110,6 +121,7 @@ static size_t json_format_ = JSON_INDENT(3) | JSON_PRESERVE_ORDER;
 typedef struct janus_mqtt_context {
 	janus_transport_callbacks *gateway;
 	MQTTAsync client;
+	char *im_host;
 	struct {
 		int mqtt_version;
 		int keep_alive_interval;
@@ -264,8 +276,11 @@ int janus_mqtt_init(janus_transport_callbacks *callback, const char *config_path
 	janus_config_category *config_status = janus_config_get_create(config, NULL, janus_config_type_category, "status");
 
 	/* Handle configuration */
-	janus_config_item *url_item = janus_config_get(config, config_general, janus_config_type_item, "url");
-	const char *url = g_strdup((url_item && url_item->value) ? url_item->value : "tcp://localhost:1883");
+	// janus_config_item *url_item = janus_config_get(config, config_general, janus_config_type_item, "url");
+	// const char *url = g_strdup((url_item && url_item->value) ? url_item->value : "tcp://localhost:1883");
+
+	janus_config_item *im_host_item = janus_config_get(config, config_general, janus_config_type_item, "im_host");
+	ctx->im_host = g_strdup((im_host_item && im_host_item->value) ? im_host_item->value : "localhost");
 
 	janus_config_item *client_id_item = janus_config_get(config, config_general, janus_config_type_item, "client_id");
 	const char *client_id = g_strdup((client_id_item && client_id_item->value) ? client_id_item->value : "guest");
@@ -275,6 +290,15 @@ int janus_mqtt_init(janus_transport_callbacks *callback, const char *config_path
 
 	janus_config_item *password_item = janus_config_get(config, config_general, janus_config_type_item, "password");
 	ctx->connect.password = g_strdup((password_item && password_item->value) ? password_item->value : "guest");
+
+	char node_host[1024];
+        memset(node_host, 0, sizeof(node_host));
+        getNodeHost(ctx->im_host, ctx->connect.username, node_host);
+	char urlbuf[1024];
+	memset(urlbuf, 0, sizeof(urlbuf));
+	snprintf(urlbuf, 1024, "tcp://%s:1883", node_host);
+
+	const char *url = g_strdup(urlbuf);
 
 	janus_config_item *json_item = janus_config_get(config, config_general, janus_config_type_item, "json");
 	if(json_item && json_item->value) {
@@ -825,6 +849,15 @@ void janus_mqtt_client_connect_failure_impl(void *context, int rc) {
 
 	/* Notify handlers about this transport failure */
 	janus_mqtt_context *ctx = (janus_mqtt_context *)context;
+
+	char node_host[1024];
+	memset(node_host, 0, sizeof(node_host));
+	getNodeHost(ctx->im_host, ctx->connect.username, node_host);
+	char urlbuf[1024];
+	memset(urlbuf, 0, sizeof(urlbuf));
+	snprintf(urlbuf, 1024, "tcp://%s:1883", node_host);
+
+
 	if(notify_events && ctx && ctx->gateway && ctx->gateway->events_is_enabled()) {
 		json_t *info = json_object();
 		json_object_set_new(info, "event", json_string("failed"));
@@ -1263,3 +1296,102 @@ int janus_mqtt_client_get_response_code5(MQTTAsync_failureData5 *response) {
 	return response ? response->code : 0;
 }
 #endif
+
+int getNodeHost(const char* host, const char* client, char* content) {
+    char buffer[BUFSIZ];
+    char result[1024];
+    enum CONSTEXPR { MAX_REQUEST_LEN = 1024};
+    char request[MAX_REQUEST_LEN];
+    struct protoent *protoent;
+    in_addr_t in_addr;
+    int request_len;
+    int socket_file_descriptor;
+    ssize_t nbytes_total, nbytes_last;
+    struct hostent *hostent;
+    struct sockaddr_in sockaddr_in;
+    unsigned short server_port = 80;
+
+
+
+    request_len = snprintf(request, MAX_REQUEST_LEN, "GET /api/node?id=%s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", client, host);
+    if (request_len >= MAX_REQUEST_LEN) {
+        fprintf(stderr, "request length large: %d\n", request_len);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Build the socket. */
+    protoent = getprotobyname("tcp");
+    if (protoent == NULL) {
+        perror("getprotobyname");
+        exit(EXIT_FAILURE);
+    }
+    socket_file_descriptor = socket(AF_INET, SOCK_STREAM, protoent->p_proto);
+    if (socket_file_descriptor == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Build the address. */
+    hostent = gethostbyname(host);
+    if (hostent == NULL) {
+        fprintf(stderr, "error: gethostbyname(\"%s\")\n", host);
+        exit(EXIT_FAILURE);
+    }
+    in_addr = inet_addr(inet_ntoa(*(struct in_addr*)*(hostent->h_addr_list)));
+    if (in_addr == (in_addr_t)-1) {
+        fprintf(stderr, "error: inet_addr(\"%s\")\n", *(hostent->h_addr_list));
+        exit(EXIT_FAILURE);
+    }
+    sockaddr_in.sin_addr.s_addr = in_addr;
+    sockaddr_in.sin_family = AF_INET;
+    sockaddr_in.sin_port = htons(server_port);
+
+    /* Actually connect. */
+    if (connect(socket_file_descriptor, (struct sockaddr*)&sockaddr_in, sizeof(sockaddr_in)) == -1) {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Send HTTP request. */
+    nbytes_total = 0;
+    while (nbytes_total < request_len) {
+        nbytes_last = write(socket_file_descriptor, request + nbytes_total, request_len - nbytes_total);
+        if (nbytes_last == -1) {
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
+        nbytes_total += nbytes_last;
+    }
+
+    /* Read the response. */
+    fprintf(stderr, "debug: before first read\n");
+    memset(result, 0, sizeof(result));
+    int dataLen = 0;
+    while ((nbytes_total = read(socket_file_descriptor, buffer, BUFSIZ)) > 0) {
+        fprintf(stderr, "debug: after a read\n");
+        write(STDOUT_FILENO, buffer, nbytes_total);
+        memcpy(result+dataLen, buffer, nbytes_total);
+        dataLen += nbytes_total;
+    }
+    result[dataLen] = 0;
+    fprintf(stderr, "debug: after last read\n");
+    fprintf(stderr, "receive msg is %s\n", result);
+
+
+    for(int i = 0; i < dataLen -4; i++) {
+      if (result[i] == '\r' && result[i+1] == '\n' && result[i+2] == '\r' && result[i+3] == '\n') {
+        memcpy(content, result+i+4, dataLen - i - 4);
+        break;
+      }
+    }
+
+    fprintf(stderr, "receive content is %s\n", content);
+
+    if (nbytes_total == -1) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+
+    close(socket_file_descriptor);
+    return 0;
+}
